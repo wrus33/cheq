015c5a86a52ce5d2a4e20fa0b5a359e46b1f2c10
William Russell <wrussell@bundystl.com>
2021-03-08 18:34:38 -0600

    add onboarding component

--- src/Onboarding.js
+++ src/Onboarding.js
@@ -1,8 +1,33 @@
 import { React, useEffect, useState } from 'react';
+import { Typography, Box, makeStyles, TextField, Button } from '@material-ui/core'
 import firebase from 'firebase'
 
+const useStyles = makeStyles({
+    root: {
+        textAlign: "center",
+        padding:100
+    },
+    headerWelcome: {
+        marginBottom:60
+    },
+    onboardPrompt: {
 
+    },
+    onboardHelper: {
 
+    },
+    form: {
+        maxWidth:500,
+        margin: "auto"
+    },
+    formElement: {
+        margin:30,
+        width:500
+    },
+    textRight: {
+        textAlign: "right"
+    },
+  });
 
 export default function Onboarding() {
 
@@ -11,6 +36,7 @@ export default function Onboarding() {
 
     //Constants
     const db = firebase.firestore();
+    const classes = useStyles();
 
     useEffect(() => {
         db.collection("sellers")
@@ -26,7 +52,34 @@ export default function Onboarding() {
     }, [db]);
 
     return (
-        <>{sellers.map(seller => seller.value.name)}</>
+        //<>{sellers.map(seller => seller.value.name)}</>
+        <Box className={classes.root}>
+            <Typography className={classes.headerWelcome} variant="h1">Welcome to cheqOS.</Typography>
+            <Typography className={classes.onboardPrompt} variant="h3">What is the name of your business?</Typography>
+            <Box className={classes.onboardHelper} fontStyle="italic"><Typography fontStyle="italic" variant="h5">(You can use your own name if operating as an individual.)</Typography></Box>
+            <div className={classes.form}>
+                <TextField
+                    className={classes.formElement}
+                    id="standard-helperText"
+                    required
+                    label="Required"
+                    helperText="You can change this at any time."
+                    variant="outlined"
+                    /><br/>
+                <div className={classes.textRight}>
+                    <Button
+                    variant="contained"
+                    color="primary"
+                // onClick={handleNext}
+                    className={classes.button}
+                >Next</Button>
+                </div> 
+            </div>
+            
+            
+        </Box>
+        
+
     )
 
 }
